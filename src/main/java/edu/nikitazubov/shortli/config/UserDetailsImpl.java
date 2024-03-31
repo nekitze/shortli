e264dd31f42ec949a38e48f28d9884af1b421497
kossmali <kossmali@student.21-school.ru>
2024-03-31 13:43:53 +0300

    Remove unused

--- src/main/java/edu/nikitazubov/shortli/config/UserDetailsImpl.java
+++ src/main/java/edu/nikitazubov/shortli/config/UserDetailsImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class UserDetailsImpl implements UserDetails {
