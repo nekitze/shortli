3fae20568e39701e73bb0d720ba91a1486e37638
kossmali <kossmali@student.21-school.ru>
2024-04-08 11:32:14 +0300

    favicon move

--- src/main/java/edu/nikitazubov/shortli/config/ThymeleafConfig.java
+++ src/main/java/edu/nikitazubov/shortli/config/ThymeleafConfig.java
@@ -4,6 +4,11 @@ import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Collections;
 
 @Configuration
 public class ThymeleafConfig {
@@ -26,5 +31,21 @@ public class ThymeleafConfig {
     @Bean
     public LayoutDialect layoutDialect() {
         return new LayoutDialect();
+    }
+
+    @Bean
+    public SimpleUrlHandlerMapping customFaviconHandlerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Integer.MIN_VALUE);
+        mapping.setUrlMap(Collections.singletonMap(
+                "/favicon.ico", faviconRequestHandler()));
+        return mapping;
+    }
+
+    @Bean
+    protected ResourceHttpRequestHandler faviconRequestHandler() {
+        ResourceHttpRequestHandler requestHandler = new ResourceHttpRequestHandler();
+        requestHandler.setLocations(Collections.singletonList(new ClassPathResource("static/images/")));
+        return requestHandler;
     }
 }
