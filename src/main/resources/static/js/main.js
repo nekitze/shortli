b1b42b806b239eee48169bd77c049981d601c8db
kossmali <kossmali@student.21-school.ru>
2024-05-07 23:05:56 +0300

    sort update

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,14 +1,16 @@
-var sortDirections = [];
+var sortDirection = true
 
 document.querySelectorAll('#urls_table th').forEach(function (header, index) {
     if (header.classList.contains("sortable")) {
         header.addEventListener('click', function () {
-            if (sortDirections[index] === undefined) {
-                sortDirections[index] = false;
-            }
-            sortTable(index, sortDirections[index]);
 
-            sortDirections[index] = !sortDirections[index];
+            if (index !== 5) {
+                sortTable(index, sortDirection);
+            } else {
+                sortTableByDate(index, sortDirection);
+            }
+
+            sortDirection = !sortDirection;
         });
     }
 });
@@ -23,34 +25,35 @@ document.querySelectorAll('#urls_table th.sortable').forEach(function (header) {
 });
 
 function sortTable(columnIndex, reverse) {
-    var table, rows, switching, i, x, y, shouldSwitch;
-    table = document.getElementById("urls_table");
-    switching = true;
+    var table = document.getElementById("urls_table");
+    var rows = Array.from(table.rows).slice(1);
+    var sortedRows = rows.sort((a, b) => {
+        var xValue = parseInt(a.getElementsByTagName("td")[columnIndex].innerText);
+        var yValue = parseInt(b.getElementsByTagName("td")[columnIndex].innerText);
 
-    while (switching) {
-        switching = false;
-        rows = table.rows;
-
-        for (i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false;
-            x = rows[i].getElementsByTagName("td")[columnIndex];
-            y = rows[i + 1].getElementsByTagName("td")[columnIndex];
-
-            const xDateValue = Date.parse(x.innerHTML);
-            const yDateValue = Date.parse(y.innerHTML);
-
-            if(!isNaN(xDateValue) || !isNaN(yDateValue)) {
-                if (reverse ? xDateValue > yDateValue : xDateValue < yDateValue) {
-                    shouldSwitch = true;
-                    break;
-                }
-            }
+        if (isNaN(xValue)) {
+            xValue = a.getElementsByTagName("td")[columnIndex].innerText;
         }
-        if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
+        if (isNaN(yValue)) {
+            yValue = b.getElementsByTagName("td")[columnIndex].innerText;
         }
-    }
+
+        return reverse? yValue - xValue : xValue - yValue;
+    });
+
+    sortedRows.forEach(row => table.appendChild(row));
+}
+
+function sortTableByDate(columnIndex, reverse) {
+    var table = document.getElementById("urls_table");
+    var rows = Array.from(table.rows).slice(1); // Преобразование строк таблицы в массив объектов
+    var sortedRows = rows.sort((a, b) => {
+        var xDateValue = Date.parse(a.getElementsByTagName("td")[columnIndex].innerHTML);
+        var yDateValue = Date.parse(b.getElementsByTagName("td")[columnIndex].innerHTML);
+        return reverse? yDateValue - xDateValue : xDateValue - yDateValue;
+    });
+
+    sortedRows.forEach(row => table.appendChild(row));
 }
 
 function copyToClipboard(id) {
